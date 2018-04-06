ce90848807ee3b58eb3cb3a6bcc11b3d834b3389
Neil Seejoor <nvseejoor@gmail.com>
2018-04-06 01:20:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
 
     if req.path.match(/items/)
           item_name = req.path.split("/items/").last
+          binding.pry
           if item = @@items.find{|i| i.name == item_name}
             resp.write item.price
           else
