386b92dd64224544cc720c521fdf5d4bde6e0f8f
Neil Seejoor <nvseejoor@gmail.com>
2018-04-06 01:20:04 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,14 +6,14 @@ class Application
 
     if req.path.match(/items/)
           item_name = req.path.split("/items/").last
-          if item =@@items.find{|i| i.name == item_name}
+          if item = @@items.find{|i| i.name == item_name}
             resp.write item.price
           else
             resp.status = 400
             resp.write "Item not found"
           end
         else
-          resp.status=404
+          resp.status = 404
           resp.write "Route not found"
         end
         resp.finish
